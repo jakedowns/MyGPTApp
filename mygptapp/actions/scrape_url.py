bc37695d61de2e1892026f33bab7a025d918146e
jake <mail@jakedowns.com>
2023-03-07 16:36:00 -0800

    synchronous scraping

--- mygptapp/actions/scrape_url.py
+++ mygptapp/actions/scrape_url.py
@@ -6,16 +6,50 @@ from mygptapp import rules, db
 from mygptapp.models import Message
 from mygptapp.openai_api import OpenAIAPI
 
+from mygptapp.actions.pyppeteer_scraper import PyppeteerScraper
+
 ai_api = OpenAIAPI()
 
+#DEFAULT_MODE = "pyppeteer" #broken
+DEFAULT_MODE = "requests"
 class ScrapeURL:
-    def __init__(self, url):
+    def __init__(self, url, mode = DEFAULT_MODE):
         self.url = url
+        self.mode = mode
         self.title = None
         self.text = None
-        self.scrape(url)
 
-    def scrape(self,url):
+    async def scrape(self, url):
+        if self.mode == "pyppeteer":
+            return await self.scrape_pyppeteer(url)
+        #elif self.mode == "requests":
+        else:
+            return self.scrape_requests(url)
+
+    def scrape_pyppeteer_fg(self, url):
+        # scrape the url and set the title and text attributes
+        # use the pyppeteer library
+        scraper = PyppeteerScraper()
+        scraper.init_sync()
+        html = scraper.scrape_sync(url)
+        soup = BeautifulSoup(html, "html.parser")
+        # if title is found, set it
+        if soup.title and "string" in soup.title:
+            self.title = soup.title.string
+        self.text = soup.get_text()
+
+    async def scrape_pyppeteer(self, url):
+        # scrape the url and set the title and text attributes
+        # use the pyppeteer library
+        scraper = PyppeteerScraper()
+        html = await scraper.scrape_in_background(url)
+        soup = BeautifulSoup(html, "html.parser")
+        # if title is found, set it
+        if soup.title and "string" in soup.title:
+            self.title = soup.title.string
+        self.text = soup.get_text()
+
+    def scrape_requests(self,url):
         # scrape the url and set the title and text attributes
         # use the requests and BeautifulSoup libraries
         response = requests.get(url)
@@ -24,6 +58,12 @@ class ScrapeURL:
         if soup.title and "string" in soup.title:
             self.title = soup.title.string
         self.text = soup.get_text()
+
+    def get_response(self):
+        return {
+            "title": self.title,
+            "text": self.text
+        }
 
     def process_scrape_results(self, original_prompt):
         # convert the self object to a message
